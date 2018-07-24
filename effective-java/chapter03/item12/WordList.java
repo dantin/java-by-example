8eaf8a2cacb2bc2f3c5f1e3f6839a48571f614ed
dantin <chengjie.ding@gmail.com>
2018-07-24 16:18:49 +0800

    item#12 implement Comparable interface.

--- effective-java/chapter03/item12/WordList.java
+++ effective-java/chapter03/item12/WordList.java
@@ -1,12 +1,16 @@
-package item12;
+// version 1.0 2018-07-24
 
 import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.HashSet;
 
 public class WordList {
     public static void main(String[] args) {
         Set<String> s = new TreeSet<String>();
+        Collections.addAll(s, args);
+        System.out.println(s);
+        s = new HashSet<String>();
         Collections.addAll(s, args);
         System.out.println(s);
     }
