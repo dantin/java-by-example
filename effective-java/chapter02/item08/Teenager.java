69d4f76544c67f7f4c4942d4e83acdd6790134fe
David Ding <chengjie.ding@gmail.com>
2019-11-25 10:43:40 +0800

    finish chapter 02. (#17)

---
+++ effective-java/chapter02/item08/Teenager.java
@@ -0,0 +1,10 @@
+
+public class Teenager {
+    public static void main(String[] args) {
+        new Room(99);
+        System.out.println("Peace out");
+
+        // Uncomment next line, note that you MUST NOT depend on this behavior!
+        System.gc();
+    }
+}
